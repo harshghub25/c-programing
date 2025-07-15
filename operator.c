2fbae031200eb3edec16743db347bee5e25dd66d
harshghub25 <deskharsh2725@hotmail.com>
2025-07-15 10:24:30 +0530

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,18 @@
+#include<stdio.h.>
+void main()
+{
+   int a=10;
+   int b=20;
+    int c;
+    int d;
+    int e;
+    int f;
+    c=a+b;
+    d=a*b;
+    e=a-b;
+    f=a/b;
+    printf("%d",c);
+    printf("\n %d",d);
+    printf("\n %d",e);
+    printf("\n %d",f);
+}
